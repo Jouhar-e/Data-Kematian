784c709b64b0d440f4bcda989b2cadade699eced
Jouhar <58057718+Jouhar-e@users.noreply.github.com>
2024-05-27 17:18:11 +0700

    Add files via upload

---
+++ webDataKematian/beranda/diagram_jenis_kelamin.php
@@ -0,0 +1,47 @@
+<?php
+$sql = "SELECT * FROM penduduk";
+$count = $db->getCount($sql);
+// echo $count;
+?>
+
+<div class="bg-light shadow-lg" style="height: 200px; width: 400px;">
+    <div class="p-5">
+        <h1><?php echo $count ?></h1>
+        <h3>Total Jumlah Penduduk</h3>
+    </div>
+</div>
+
+<!-- <div>
+    <canvas id="chartJenisKelamin" width="400" height="400"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctx = document.getElementById('chartJenisKelamin').getContext('2d');
+    var chartJenisKelamin = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Laki-laki', 'Perempuan'],
+            datasets: [{
+                label: 'Jumlah',
+                data: [750, 65], // Ubah nilai sesuai dengan data Anda
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script> -->
---
+++ webDataKematian/index.php
@@ -0,0 +1,58 @@
+<?php
+require_once "module.php";
+$db = new DB;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="image/naruto 1.jpg" alt="" width="30" height="24">
+            </a>
+            <a class="navbar-brand" href="#">JWSM</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="?f=beranda&m=beranda">Home</a>
+                    <a class="nav-link" href="?f=penduduk&m=select">Data Penduduk</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="col mt-4">
+                    <?php
+                    if (isset($_GET['f']) && isset($_GET['m'])) {
+                        $f = $_GET['f'];
+                        $m = $_GET['m'];
+
+                        $file = $f . "/" . $m . ".php";
+                        require_once $file;
+                    } else {
+                        require_once "beranda/beranda.php";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>
