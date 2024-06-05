d923967e9fb1b9575b9ae09e8c11db79101fd2f4
Jouhar-e <jauharibaru6@gmail.com>
2024-06-05 17:40:45 +0700

    new database

--- data-kematian.sql
+++ data-kematian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 27, 2024 at 10:16 AM
+-- Generation Time: Jun 03, 2024 at 11:46 AM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.33
 
@@ -44,6 +44,30 @@ INSERT INTO `penduduk` (`idpenduduk`, `penduduk`, `nik`, `tgllahir`, `jk`, `alam
 (3, 'Sulistiono', '3523061710600001', '2024-05-24', 'Laki-Laki', 'RT 3 RW 4 Desa Senori'),
 (4, 'Wahyu Firmansyah', '3523061710600001', '2024-05-27', 'Laki-Laki', 'RT 3 RW 4 Desa Merakurak');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `idpetugas` int NOT NULL,
+  `petugas` varchar(255) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(50) NOT NULL,
+  `status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`idpetugas`, `petugas`, `telp`, `email`, `password`, `level`, `status`) VALUES
+(1, 'Admin', '085974104374', 'admin@gmail.com', '123', 'Admin', 1),
+(3, 'Mulyadi', '087812762899', 'mulyadi@gmail.com', '123', 'Petugas', 1);
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +79,12 @@ ALTER TABLE `penduduk`
   ADD PRIMARY KEY (`idpenduduk`);
 
 --
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`idpetugas`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +93,12 @@ ALTER TABLE `penduduk`
 --
 ALTER TABLE `penduduk`
   MODIFY `idpenduduk` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `idpetugas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
